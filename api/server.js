e829288b7c6c484857dba7e15396d47e566e7853
Vitalik Kim <kimv1086@gmail.com>
2020-02-22 12:00:05 +0600

    create server port

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port`);
+});
