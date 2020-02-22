413bc93179112245fce2c5ea7a19cd5f92fba5fd
Vitalik Kim <kimv1086@gmail.com>
2020-02-22 14:58:44 +0600

    create reducer

---
+++ frontend/src/store/actions/actions.js
@@ -0,0 +1,124 @@
+import axios from "../../axios-api";
+import {FETCH_NEWS_SUCCESS, FETCH_FAILURE, CREATE_NEW_NEWS_SUCCESS, FETCH_ONE_NEWS_SUCCESS, DELETE, FETCH_COMMENTS_SUCCESS, CREATE_COMMENT_SUCCESS, FETCH_ONE_COMMENT_SUCCESS} from "./actionTypes";
+import {NotificationManager} from "react-notifications";
+
+export const fetchNewsSuccess = news => {
+  return {type: FETCH_NEWS_SUCCESS, news};
+};
+export const fetchFailure = error => ({type: FETCH_FAILURE, error});
+
+export const createNewNewsSuccess = () => ({type: CREATE_NEW_NEWS_SUCCESS});
+
+export const fetchOneNewsSuccess = (news, id) => {
+  return {type: FETCH_ONE_NEWS_SUCCESS, news, id};
+};
+
+export const fetchCommentsSuccess = comments => {
+  return {type: FETCH_COMMENTS_SUCCESS, comments};
+};
+
+export const fetchOneCommentSuccess = (comment) => {
+  return {type: FETCH_ONE_COMMENT_SUCCESS, comment};
+};
+
+export const createCommentSuccess = () => ({type: CREATE_COMMENT_SUCCESS});
+
+export const createNotification = (type) => {
+  return () => {
+    switch (type) {
+      case 'success':
+        NotificationManager.success("Success", 'Success');
+        break;
+      case 'error':
+        NotificationManager.error("Error", 'Database error 500', 5000);
+        break;
+      default:
+        break;
+    }
+  };
+};
+
+
+export const fetchNews = () => {
+  return dispatch => {
+    return axios.get('/news').then(
+      response => {
+        dispatch(fetchNewsSuccess(response.data))
+      },
+      error => {
+        dispatch(fetchFailure(error));
+        dispatch(createNotification('error'));
+      }
+    );
+  };
+};
+
+export const createNews = data => {
+  return dispatch => {
+    return axios.post('/news', data).then(
+      () => {
+        dispatch(createNewNewsSuccess())
+      }, error => {
+        dispatch(fetchFailure(error));
+      }
+    );
+  };
+};
+
+export const openOneNews = id => {
+  return dispatch => {
+    return axios.get('/news/' + id).then(
+      response => {
+        dispatch(fetchOneNewsSuccess(response.data, id))
+      }, error => {
+        dispatch(fetchFailure(error));
+        dispatch(createNotification('error'));
+      }
+    );
+  };
+};
+
+export const fetchDelete = (id) => {
+  return dispatch => {
+    axios.delete('/news/' + id).then(() => {
+      dispatch({type: DELETE});
+      dispatch(fetchNews());
+    })
+  };
+};
+
+
+export const fetchComments = (id) => {
+  return dispatch => {
+    return axios.get('/comments/' + id).then(
+      response => {
+        dispatch(fetchCommentsSuccess(response.data))
+      },
+      error => {
+        dispatch(fetchFailure(error));
+        dispatch(createNotification('error'));
+      }
+    );
+  };
+};
+
+export const createComment = data => {
+  return dispatch => {
+    return axios.post('/comments', data).then(
+      () => {
+        dispatch(createCommentSuccess())
+      }, error => {
+        dispatch(fetchFailure(error));
+      }
+    );
+  };
+};
+
+export const fetchDeleteComment = (id) => {
+  return dispatch => {
+    axios.delete('/comments/' + id).then(() => {
+      dispatch({type: DELETE});
+      dispatch(fetchComments());
+    })
+  };
+};
---
+++ frontend/src/store/reducer/reducers.js
@@ -0,0 +1,52 @@
+import {
+  FETCH_NEWS_SUCCESS,
+  FETCH_FAILURE,
+  FETCH_ONE_NEWS_SUCCESS,
+  DELETE,
+  FETCH_COMMENTS_SUCCESS,
+  FETCH_ONE_COMMENT_SUCCESS
+} from "../actions/actionTypes";
+
+const initialState = {
+  news: [],
+  newsId: null,
+  oneNews: null,
+  comments: [],
+  oneComment: [],
+  error: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_NEWS_SUCCESS:
+      return {...state, news: action.news};
+    case FETCH_FAILURE:
+      return {
+        ...state,
+        error: action.error
+      };
+    case FETCH_ONE_NEWS_SUCCESS:
+      return {
+        ...state,
+        oneNews: action.news,
+        newsId: action.id
+      };
+    case DELETE:
+      return {
+        ...state,
+        oneNews: null,
+        error: action.error
+      };
+    case FETCH_COMMENTS_SUCCESS:
+      return {...state, comments: action.comments};
+    case FETCH_ONE_COMMENT_SUCCESS:
+      return {
+        ...state,
+        oneComment: action.comment
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
