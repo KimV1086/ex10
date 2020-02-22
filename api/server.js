6fc68042715bad2164b3a96da1883065ce6fc725
Vitalik Kim <kimv1086@gmail.com>
2020-02-22 13:06:00 +0600

    create SQL data base

--- api/server.js
+++ api/server.js
@@ -3,15 +3,25 @@ const app = express();
 
 const news = require('./app/news');
 
+const mysql = require('mysql');
 const cors = require('cors');
+
+const port = 7000;
 
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cors());
 
-app.use('news', news);
 
-const port = 8000;
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1qaz@WSX29',
+  database : 'exam_10'
+});
+
+
+app.use('news', news(connection));
 
 app.listen(port, () => {
   console.log(`Server started on ${port} port`);
