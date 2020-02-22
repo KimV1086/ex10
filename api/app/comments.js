283cea7ef0daa9a4c86810886d9d879ff922d88c
Vitalik Kim <kimv1086@gmail.com>
2020-02-22 13:28:45 +0600

    added comments inquiries

---
+++ api/app/comments.js
@@ -0,0 +1,76 @@
+const multer  = require('multer');
+const path = require('path');
+const config = require('../config');
+const express = require('express');
+const nanoid = require('nanoid');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, config.uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, nanoid() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({storage});
+
+const createRouter = connection => {
+  const router = express.Router();
+
+  router.get('/', (req, res) => {
+    connection.query('SELECT * FROM `comments`', (error, results) => {
+      if(error) {
+        res.status(500).send({error: 'Database error'})
+      }
+      res.send(results);
+    });
+  });
+
+  router.get('/:id', (req, res) => {
+    connection.query('SELECT * FROM `comments` WHERE `news_id` = ?', req.params.id, (error, results) => {
+      if(error) {
+        res.status(500).send({error: 'Database error'})
+      }
+      if(results[0]) {
+        res.send(results[0]);
+      } else {
+        res.status(404).send({error: 'Comments not found'})
+      }
+
+    });
+  });
+
+  router.post('/', upload.single('image'), (req, res) => {
+    const comments = req.body;
+
+    if(req.file){
+      news.image = req.file.filename;
+    }
+
+    connection.query('INSERT INTO `comments` (`id`, `news_id`, `author`, `comment`) VALUES (?, ?, ?, ?)',
+      [comments.id, comments.news_id, comments.author, comments.comment],
+      (error, results) => {
+        if(error) {
+
+          res.status(500).send({error: 'Database error'})
+        }
+        res.send({message: "Ok"});
+      }
+    );
+  });
+
+  router.delete('/:id', (req, res) => {
+    connection.query('DELETE FROM `comments` WHERE `id` = ?', req.params.id, (error) => {
+      if (error) {
+        res.status(500).send({error: error.sqlMessage});
+      }
+      res.send({message: "Success"});
+    });
+  });
+
+  return router;
+};
+
+
+module.exports = createRouter;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const news = require('./app/news');
+const comments = require('./app/comments');
 
 const mysql = require('mysql');
 const cors = require('cors');
@@ -21,8 +22,18 @@ const connection = mysql.createConnection({
 });
 
 
-app.use('news', news(connection));
+app.use('/news', news(connection));
+app.use('/comments', comments(connection));
 
-app.listen(port, () => {
-  console.log(`Server started on ${port} port`);
+connection.connect((err) =>{
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port`);
+  });
 });
