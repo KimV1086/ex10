a3af06f95a33edd056c35f21db7ba6171c6c66bd
Vitalik Kim <kimv1086@gmail.com>
2020-02-22 12:30:32 +0600

    added news inquiries

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const app = express();
+
+const news = require('./app/news');
+
 const cors = require('cors');
 
-const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cors());
+
+app.use('news', news);
 
 const port = 8000;
 
